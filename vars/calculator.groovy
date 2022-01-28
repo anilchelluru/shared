92d9d2a2af0368897d9d03189d1a291227e02ad7
anil <93664826+anilchelluru@users.noreply.github.com>
2022-01-28 17:08:42 +0530

    Create calculator.groovy

---
+++ vars/calculator.groovy
@@ -0,0 +1,11 @@
+def add(x,y){
+  echo "sum of ${x} and ${y} is ${x+y}"
+}
+
+def multiply(x,y){
+  echo "multiplication of ${x} and ${y} is ${x*y}"
+}
+
+def subtraction(x,y){
+  echo "subtraction of ${x} and ${y} is ${x-y}"
+}
